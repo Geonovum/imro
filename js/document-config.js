da7ac74f89667f8dbbb0aec0eaadf1c79b27c7cd
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2023-11-29 14:22:03 +0100

    update specTyoe

--- js/document-config.js
+++ js/document-config.js
@@ -4,7 +4,7 @@ var documentConfig =
   shortName: "imro",
   pubDomain: "ro",
   specStatus: "def",
-  specType: "no",
+  specType: "im",
   license: "cc-by-nd",
   latestVersion: [
     "https://docs.geostandaarden.nl/ro/imro/"
