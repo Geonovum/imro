091e7ffab1454fe466106f1cea49f5a6fb4a7388
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2023-11-29 13:51:41 +0100

    update config.js naar dv versie

--- js/document-config.js
+++ js/document-config.js
@@ -1,21 +1,21 @@
 var documentConfig =
 {
   title: "Informatiemodel Ruimtelijke Ordening 2012 (IMRO) versie 1.2.1",
-  subtitle: "Geonovum Informatiemodel"
   shortName: "imro",
   pubDomain: "ro",
-  specStatus: "wv",
+  specStatus: "dv",
   specType: "no",
   license: "cc-by-nd",
   latestVersion: [
     "https://docs.geostandaarden.nl/ro/imro/"
   ],
-  publishDate: "2023-11-01",
+  publishDate: "2023-11-29",
   publishVersion: "1.2.1",
   thisVersion: [
     "https://geonovum.github.io/imro/"
   ],
   previousPublishDate: "2012-04-18",
+  previousPublishVersion: "1.2.0",
   prevVersion: [
     "https://ro-standaarden.geonovum.nl/2012/IMRO/1.2/IMRO2012-v1.2.pdf"
   ],
